4def7b7124430b73c374574e6f05d6c9120c431a
Vladyslav Popov <popovvladislav4891@gmail.com>
2025-03-13 18:29:41 +0200

    Add interactive mode implementation

--- QESolver.js
+++ QESolver.js
@@ -1,3 +1,25 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Enter a: ', (a) => {
+    rl.question('Enter b: ', (b) => {
+        rl.question('Enter c: ', (c) => {
+            a = Number(a);
+            b = Number(b);
+            c = Number(c);
+
+            console.log(`Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0`);
+
+            findRoots(a, b, c);
+            rl.close();
+        })
+    })
+})
+
 const findRoots = (a, b, c) => {
     const discriminant = b ** 2 - 4 * a * c;
     let x1, x2;
@@ -5,13 +27,11 @@ const findRoots = (a, b, c) => {
     if (discriminant > 0) {
         x1 = (-b + Math.sqrt(discriminant)) / 2 * a;
         x2 = (-b - Math.sqrt(discriminant)) / 2 * a;
-        console.log(`x1 = ${x1}, x2 = ${x2}`);
+        console.log(`There are 2 roots\n x1 = ${x1}\n x2 = ${x2}`);
     } else if (discriminant === 0) {
-        x1 = x2 = -b / 2 * a;
-        console.log(`x1 = ${x1}, x2 = ${x2}`);
+        x1 = -b / 2 * a;
+        console.log(`There are 1 roots\n x1 = ${x1}`);;
     } else {
-        return console.log("There is no roots to find")
+        return console.log("There are 0 roots");
     }
 }
-
-findRoots(1, -26, 120);
